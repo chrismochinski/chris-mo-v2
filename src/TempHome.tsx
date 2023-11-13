b2e3f7904c0338faacb5555e24e4a34ac473978c
Chris Mochinski <cmochinski@gmail.com>
2023-11-13 01:21:45 -0600

    scary console log stuff

--- src/TempHome.tsx
+++ src/TempHome.tsx
@@ -6,16 +6,44 @@ import "./TempHome.scss";
 export function TempHome(): JSX.Element {
   const [introIsTyped, setIntroIsTyped] = useState(false);
 
+  function startBoomBoom() {
+    setTimeout(() => {
+      console.log("%cit's time", "color: #ff000088; font-style: italic; font-weight: 200; font-size: 8px;");
+    }, 400);
+    setTimeout(() => {
+      console.log("%care you ready?", "color: #ff0000cf; font-style: italic; font-weight: 400; font-size: 12px;");
+    }, 1400);
+    setIntroIsTyped(true);
+  }
+
   return (
     <div className="tempHome">
       <header className="tempHomeHeader">
         <img src={logo} className="tempHomeLogo" alt="logo" />
+        {/* <p className="waitForItText">Edit src/App.js and save to reload</p> */}
         <TypeAnimation
-          sequence={["wait for it", 1000, () => setIntroIsTyped(true)]}
+          sequence={[
+            "Edit src/App.js and save to reload",
+            2000,
+            "actually...",
+            3000,
+            "you know what?",
+            3000,
+            "wait for it",
+            400,
+            () => startBoomBoom(),
+          ]}
           wrapper="p"
           className="waitForItText"
           preRenderFirstString
         />
+        <a
+          className="defaultLink"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
       </header>
     </div>
   );
