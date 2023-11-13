0b0b7b6340a074be8c02f0dfe265cc79ab954ee5
Chris Mochinski <cmochinski@gmail.com>
2023-11-13 00:57:20 -0600

    basic typing

--- src/TempHome.tsx
+++ src/TempHome.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 import logo from "./logo.svg";
 import "./TempHome.scss";
-
 
 export function TempHome(): JSX.Element {
   return (
     <div className="tempHome">
       <header className="tempHomeHeader">
         <img src={logo} className="tempHomeLogo" alt="logo" />
-        <p>wait for it<span>_</span></p>
+        <TypeAnimation
+          sequence={["wait for it", 1000]}
+          wrapper="p"
+          className="waitForItText"
+          preRenderFirstString
+        />
       </header>
     </div>
   );
