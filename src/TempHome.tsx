210c26ecc79e508924f2b9f1ef9d8148b61384ef
Chris Mochinski <cmochinski@gmail.com>
2023-11-13 00:58:20 -0600

    callback intro istyped

--- src/TempHome.tsx
+++ src/TempHome.tsx
@@ -4,12 +4,14 @@ import logo from "./logo.svg";
 import "./TempHome.scss";
 
 export function TempHome(): JSX.Element {
+  const [introIsTyped, setIntroIsTyped] = useState(false);
+
   return (
     <div className="tempHome">
       <header className="tempHomeHeader">
         <img src={logo} className="tempHomeLogo" alt="logo" />
         <TypeAnimation
-          sequence={["wait for it", 1000]}
+          sequence={["wait for it", 1000, () => setIntroIsTyped(true)]}
           wrapper="p"
           className="waitForItText"
           preRenderFirstString
