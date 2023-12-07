912370c743788e895122074cff545a66dccc3aca
Chris Mochinski <cmochinski@gmail.com>
2023-12-07 11:26:08 -0600

    starting bulb body page

---
+++ src/BulbBody/BulbBody.tsx
@@ -0,0 +1,8 @@
+export function BulbBody(): JSX.Element {
+    return (
+        <div className="bulbBodyWrapper">
+
+        <img src="https://i.imgur.com/5r5YJXp.png" alt="bulb body" className="bulbBody" />
+        </div>
+    )
+}
