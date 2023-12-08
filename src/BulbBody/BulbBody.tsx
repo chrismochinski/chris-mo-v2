5fd945728f608186f1163ac80097f13e75908be2
Chris Mochinski <cmochinski@gmail.com>
2023-12-08 11:34:36 -0600

    bulb container

--- src/BulbBody/BulbBody.tsx
+++ src/BulbBody/BulbBody.tsx
@@ -1,8 +1,13 @@
-export function BulbBody(): JSX.Element {
-    return (
-        <div className="bulbBodyWrapper">
+import bulb1Off from "../assets/bulbs/bulb-1-off.png";
+import bulb1On from "../assets/bulbs/bulb-1-on.png";
 
-        <img src="https://i.imgur.com/5r5YJXp.png" alt="bulb body" className="bulbBody" />
-        </div>
-    )
+export function BulbBody(): JSX.Element {
+  return (
+    <div className="bulbBodyWrapper">
+      <div className="bulb1Container">
+        <img id="bulb1Off" className="bulb bulb1" src={bulb1Off} alt="bulb 1 - far left" />
+        <img id="bulb1On" className="bulb bulb1" src={bulb1On} alt="bulb 2 - middle left" />
+      </div>
+    </div>
+  );
 }
