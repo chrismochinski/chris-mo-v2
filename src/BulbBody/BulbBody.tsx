6609105ca90d464ea3bffad3b6943b9e6a7528f3
Chris Mochinski <cmochinski@gmail.com>
2023-12-08 11:44:52 -0600

    bulb placed! well, one

--- src/BulbBody/BulbBody.tsx
+++ src/BulbBody/BulbBody.tsx
@@ -1,12 +1,13 @@
 import bulb1Off from "../assets/bulbs/bulb-1-off.png";
 import bulb1On from "../assets/bulbs/bulb-1-on.png";
+import "./BulbBody.scss";
 
 export function BulbBody(): JSX.Element {
   return (
     <div className="bulbBodyWrapper">
       <div className="bulb1Container">
-        <img id="bulb1Off" className="bulb bulb1" src={bulb1Off} alt="bulb 1 - far left" />
-        <img id="bulb1On" className="bulb bulb1" src={bulb1On} alt="bulb 2 - middle left" />
+        <img id="bulb1Off" className="bulb bulb1" src={bulb1Off} alt="bulb 1 - far left - POWERED OFF" />
+        <img id="bulb1On" className="bulb bulb1" src={bulb1On} alt="bulb 1 - far left - POWERED ON" />
       </div>
     </div>
   );
